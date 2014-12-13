b2e2a1b57c442987c631d954d78e1121fd6ab3d3
Denis Pavlyutchenko <Hixon10@yandex.ru>
2014-12-13 23:17:00 +0300

    Добавил тесты

---
+++ tests/TestYandexDiskRestClient.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+__author__ = 'Denis'
+
+
+class TestYandexDiskRestClient(TestCase):
+    def test_get_disk_metadata(self):
+        self.fail()
+
+    def test_get_content_of_folder(self):
+        self.fail()
+
+    def test_create_folder(self):
+        self.fail()
+
+    def test_remove_folder_or_file(self):
+        self.fail()
+
+    def test_copy_folder_of_file(self):
+        self.fail()
+
+    def test_get_download_link_to_file(self):
+        self.fail()
+
+    def test_get_published_files(self):
+        self.fail()
+
+    def test_get_public_link_to_folder_or_file(self):
+        self.fail()
+
+    def test_unpublish_folder_or_file(self):
+        self.fail()
+
+    def test_get_list_of_all_files(self):
+        self.fail()
+
+    def test_move_folder_of_file(self):
+        self.fail()
+
+    def test_upload_file(self):
+        self.fail()
+
+    def test_upload_file_from_url(self):
+        self.fail()
+
+    def test__get_dictionary_of_published_files(self):
+        self.fail()
+
+    def test__check_code(self):
+        self.fail()
