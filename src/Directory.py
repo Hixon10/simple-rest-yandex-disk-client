11336da43ded5e824e3616b32a4b2588e2143565
Denis Pavlyutchenko <Hixon10@yandex.ru>
2014-12-16 02:22:20 +0300

    Bug fix

--- src/Directory.py
+++ src/Directory.py
@@ -4,14 +4,14 @@ from src.File import File
 
 
 class Directory():
-    def __init__(self, _embedded=None, **kwargs):
+    def __init__(self, **kwargs):
         self.children = []
 
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
-        if _embedded is not None:
-            for item in _embedded['items']:
+        if "_embedded" in kwargs:
+            for item in kwargs["_embedded"]['items']:
                 if item["type"] == "dir":
                     d = Directory(**item)
                     self.children.append(d)
